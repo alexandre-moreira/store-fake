8365f0c91147c35a305c23498f998a77d396da28
Alexandre Moreira <4lexandre.moreira@gmail.com>
2020-11-09 18:01:05 -0300

    Begin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Routes from "./Routes";
+import Template from "./components/Template";
+//import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <Template>
+      <Routes />
+    </Template>
+    );
+}
+
+export default App;
